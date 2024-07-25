01479deef38574480580a3641a2ab958fed8220c
Juan Pablo <jpmonteagudo2014@gmail.com>
2024-07-25 15:49:09 -0400

    adding geom.mean,sd,var and z-score

--- R/geometric_central.R
+++ R/geometric_central.R
@@ -88,3 +88,126 @@ geom.mad <- function(x,
     return(mad)
   }
 }
+
+#> Calculate the geometric mean for a set of observations.
+#> The geometric mean is the nth root of the product of 'n' observations.
+#> It multiplies rather than sums the observations in a set and
+#> can be thought of as the exponential of the arithmetic mean of logarithms.
+#> We use  this definition to create our function.
+#>
+#> The 'geom.mean' function will calculate the mean for all numeric columns in
+#> a matrix or data frame.
+#>
+#> @params x: a numeric vector, matrix or data frame
+#> @params ... additional parameters to be passed to 'mean()'
+#> @params na.rm: indicate whether or not to remove NAs
+#>
+#> NOTE: Because we're using logarithms, our observations must be non-negative.
+#> Additionally, by using the geometric mean and SD, we'll be measuring the
+#> log-normal dispersion of a log-normal distribution,
+#>
+geom.mean <- function(x, ..., na.rm = FALSE) {
+  if (is.data.frame(x) || is.matrix(x)) {
+    if (na.rm) {
+      x <- apply(x, 2, function(column) column[!is.na(column)])
+    }
+    geom_mean <- apply(x, 2, function(column) {
+      g.mean <- exp(mean(log(column)))
+    })
+    return(geom_mean)
+  } else {
+    if (na.rm) {
+      x <- x[!is.na(x)]
+    }
+    g.mean <- exp(mean(log(x)))
+    return(g.mean)
+  }
+}
+
+#> Calculate the geometric standard deviation (GSD) of the geometric mean. the GSD
+#> is a dimensionless (no units as output) multiplicative factor. The GSD describes
+#> the range from the mean divided by GSD to the mean multiplied by GSD. Because of
+#> its multiplicative nature, the GSD cannot be added/subtracted from the mean.
+#>
+#> @params x: a numeric vector, matrix or data frame
+#> @params ... additional parameters to be passed to 'geom.mean()'
+#> @params na.rm: indicate whether or not to remove NAs
+
+geom.sd <- function(x,...,na.rm = FALSE){
+  if (is.data.frame(x) || is.matrix(x)) {
+    if (na.rm) {
+      x <- apply(x, 2, function(column) column[!is.na(column)])
+    }
+    geom_sd <- apply(x, 2, function(column) {
+      n <- length(column)
+      g_mean <- geom.mean(column)
+      g.sd <- exp(sqrt(sum((log(column/g_mean))^2) / n))
+    })
+    return(geom_sd)
+  } else {
+    if (na.rm) {
+      x <- x[!is.na(x)]
+    }
+    n <- length(x)
+    g_mean <- geom.mean(x)
+    g.sd <- exp(sqrt(sum((log(x/g_mean))^2) / n))
+    return(g.sd)
+  }
+}
+
+#> Calculate the geometric variance of a set of observations. The variance is also
+#> a dimensionless factor.
+#>
+#> @params x: a numeric vector, matrix or data frame
+#> @params ... additional parameters to be passed to 'geom.mean()'
+#> @params na.rm: indicate whether or not to remove NAs
+
+geom.var <- function(x,...,na.rm = FALSE){
+  if (is.data.frame(x) || is.matrix(x)) {
+    if (na.rm) {
+      x <- apply(x, 2, function(column) column[!is.na(column)])
+    }
+    geom_var <- apply(x, 2, function(column) {
+      n <- length(column)
+      g_mean <- geom.mean(column)
+  g.var <- exp(sum((log(column/g_mean))^2) / n)
+  })
+    return(geom_var)
+  }  else {
+    if (na.rm) {
+      x <- x[!is.na(x)]
+    }
+    n <- length(x)
+    g_mean <- geom.mean(x)
+    g.var <- exp(sum((log(x/g_mean))^2) / n)
+    return(g.var)
+  }
+}
+#> Calculate the number of GSD by which the raw value is above/below the
+#> mean. The formula here makes use of the change of base property of logarithms
+#>
+#> @params x: a numeric vector, matrix or data frame
+#> @params ... additional parameters to be passed to 'geom.mean()'
+#> @params na.rm: indicate whether or not to remove NAs
+
+geom.zscore <- function(x,...,na.rm = FALSE){
+  if (is.data.frame(x) || is.matrix(x)) {
+    if (na.rm) {
+      x <- apply(x, 2, function(column) column[!is.na(column)])
+    }
+    geom_z <- apply(x, 2, function(column) {
+      g_mean <- geom.mean(column)
+      g_sd <- geom.sd(column)
+      g.z <- log(column/g_mean, base = g_sd)
+    })
+    return(geom_z)
+  }  else {
+    if (na.rm) {
+      x <- x[!is.na(x)]
+    }
+    g_mean <- geom.mean(x)
+    g_sd<- geom.sd(x)
+    g.z <- log(x/g_mean, base = g_sd)
+    return(g.z)
+  }
+}
